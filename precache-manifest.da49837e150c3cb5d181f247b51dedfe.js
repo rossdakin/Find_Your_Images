db6b261a068c1c4e33f1a6aea3490f715aaf3b80
Ross Dakin <rossdakin@gmail.com>
2019-05-24 17:22:38 -0400

    Updates

---
+++ precache-manifest.da49837e150c3cb5d181f247b51dedfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0710b78619e5b0f336536e3255651a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1237ba207a8615830ad",
+    "url": "/static/css/main.42cc514a.chunk.css"
+  },
+  {
+    "revision": "76d3e31d927ad487cac7",
+    "url": "/static/js/2.bc7e3794.chunk.js"
+  },
+  {
+    "revision": "d1237ba207a8615830ad",
+    "url": "/static/js/main.f1c048e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/navbar/NavBar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import AppBar from 'material-ui/AppBar';
-
-const NavBar = () => <AppBar title="Image Finder" />;
-    
-export default NavBar;
